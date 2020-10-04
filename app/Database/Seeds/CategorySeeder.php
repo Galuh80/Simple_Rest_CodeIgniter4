f8216e699d8b093d2357fc19da151af0de76c971
Galuh80 <galuh.ibrahiim@gmail.com>
2020-10-04 08:47:08 +0700

    First Commit

---
+++ app/Database/Seeds/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Seeds;
+
+class CategorySeeder extends \CodeIgniter\Database\Seeder
+{
+    public function run()
+    {
+        $data1 = [
+            'category_name'     => 'Makanan',
+            'category_status'   => 'Active'
+        ];
+        $data2 = [
+            'category_name'     => 'Minuman',
+            'category_status'   => 'Active'
+        ];
+
+        $this->db->table('categories')->insert($data1);
+        $this->db->table('categories')->insert($data2);
+    }
+}
